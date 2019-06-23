d85f41b5fe23ccbdbaae51021e88d156633ab5f2
Yustian Ray <ray.yustian@gmail.com>
2019-06-23 10:52:15 +0700

    Initial Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style.css'
+
+export default () => {
+    return (
+        <section id="showcase" className="py-5">
+            <div className="primary-overlay text-white">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12 text-center">
+                            <h1 className="display-2 mt-5 pt-5">
+                                Coming Soon
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
